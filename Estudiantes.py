f2422ebe02d287de54e0a1ab0b68f1c85f6c0594
Marvin <sonmarvint2001@gmail.com>
2025-07-16 19:02:05 -0600

    Primer borrador de ingreso

--- Estudiantes.py
+++ Estudiantes.py
@@ -1,4 +1,5 @@
 Estudiantes={}
+Cursos={}
 def Ingresar():
     try:
         cantidad=int(input("Ingrese la cantidad de estudiantes que desea agregar al sistema: "))
@@ -16,16 +17,37 @@ def Ingresar():
                     j=1
                     while j<=cantidadCursos:
                         codigoCursoAux=input("Ingrese la codigo curso que desea ingresar: ")
-                        if codigoCursoAux in Estudiantes[carreraAux]:
+                        if codigoCursoAux in Estudiantes[carreraAux][Cursos]:
                             print("El codigo ya existe en este estudiante")
                         else:
                             nombreCursoAux=input("Ingrese el nombre del curso: ")
                             notaTarea=float(input("Ingrese la nota tarea: "))
                             notaParcial=float(input("Ingrese la nota parcial: "))
                             notaProyecto=float(input("Ingrese la nota proyecto: "))
+                            if ((notaProyecto>=0) and (notaProyecto<=100)) and ((notaTarea>=0) and (notaTarea<=100)) and ((notaParcial>=0) and (notaParcial<=100)):
+                                Cursos[codigoCursoAux]={
+                                    "nombreCurso": nombreCursoAux,
+                                    "notaTarea": notaTarea,
+                                    "notaParcial": notaParcial,
+                                    "notaProyecto": notaProyecto
+                                }
+                                Estudiantes[carnetAux]={
+                                    "nombre": nombreAux,
+                                    "edad": edadAux,
+                                    "carrera": carreraAux,
+                                    "cursos": codigoCursoAux
+                                }
+                                j=j+1
+                            else:
+                                print("ERROR: TODAS LAS NOTAS DEBEN ESTAR EN UN RANGO DE 0 a 100")
+                    i=i+1
+
                 else:
                     print("ERROR: La edad debe ser un numero entero entre 16 y 100")
 
 
     except ValueError:
         print("ERROR:")
+
+    while True:
+        print("==MENU==")
